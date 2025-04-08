ae884fff9202c214588b0f01c3d149438dc26368
Ihar <rathack13@gmail.com>
2025-04-08 22:17:21 +0300

    added: database config settings

---
+++ server/src/config/dbConfig.ts
@@ -0,0 +1,39 @@
+import logger from '@utils/logger';
+import {Dialect} from "sequelize";
+
+export interface IDbConfig {
+    database: string;
+    user: string;
+    password: string;
+    host: string;
+    port: number;
+    timezone: string;
+    dialect: Dialect;
+    pool: {
+        max: number;
+        min: number;
+        acquire: number;
+        idle: number;
+    };
+    logging: (msg: string) => any;
+}
+
+
+const dbConfig: IDbConfig = {
+    database: process.env.POSTGRES_DB!,
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+    host: process.env.DB_HOST || 'localhost',
+    port: 5432,
+    timezone: '+03:00',
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    logging: (msg: string) => logger.info(msg),
+}
+
+export default dbConfig;
