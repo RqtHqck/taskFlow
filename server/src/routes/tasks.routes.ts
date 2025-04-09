c001e233b97816ebd6fd267e2e27fa1c59af7b9c
Ihar <rathack13@gmail.com>
2025-04-09 16:10:54 +0300

    Revert "added: routes, but as variant if fail"
    This reverts commit 2612930e55424568affeaf0d14e621dba62e8834.

--- server/src/routes/tasks.routes.ts
+++ server/src/routes/tasks.routes.ts
@@ -1,52 +1,52 @@
-// import { Router } from 'express';
-// import {TasksController} from "@controllers/tasks.controller";
-// import {TasksService} from "@services/tasks.service";
-//
-// const tasksService = new TasksService();
-// const tasksController = new TasksController(tasksService);
-//
-// const tasksRouter = Router();
-//
-// // POST api/v1/tasks
-// // Body {title, description, status}
-// tasksRouter.post('/',
-//     tasksController.create.bind(tasksController));
-//
-// // GET api/v1/tasks
-// // Query Params date=date&fromDate=fromDate&toDate=toDate&status=status
-// tasksRouter.get('/',
-//     tasksController.getAll.bind(tasksController));
-//
-// // GET api/v1/tasks/:taskId
-// // Params taskId
-// tasksRouter.get('/:taskId',
-//     tasksController.getById.bind(tasksController));
-//
-// // GET api/v1/tasks/:taskId/status
-// // Params taskId
-// tasksRouter.get('/:taskId',
-//     tasksController.getTaskStatus.bind(tasksController));
-//
-// // PUT api/v1/tasks/:taskId
-// // Params taskId
-// // Body {title, description, comment, status}
-// tasksRouter.put('/:taskId',
-//     tasksController.updateById.bind(tasksController));
-//
-// // PATCH api/v1/tasks/:taskId/status
-// // Params taskId
-// // Body {status, comment}
-// tasksRouter.patch('/:taskId',
-//     tasksController.updateTaskStatusWithResponse.bind(tasksController));
-//
-// // PATCH api/v1/tasks/status
-// // Body {status}
-// tasksRouter.patch('/:taskId',
-//     tasksController.updateManyStatusesToPending.bind(tasksController));
-//
-// // PATCH api/v1/tasks/status
-// // Params taskId
-// tasksRouter.delete('/:taskId',
-//     tasksController.deleteById.bind(tasksController));
-//
-// export default tasksRouter;
+import { Router } from 'express';
+import {TasksController} from "@controllers/tasks.controller";
+import {TasksService} from "@services/tasks.service";
+
+const tasksService = new TasksService();
+const tasksController = new TasksController(tasksService);
+
+const tasksRouter = Router();
+
+// POST api/v1/tasks
+// Body {title, description, status}
+tasksRouter.post('/',
+    tasksController.create.bind(tasksController));
+
+// GET api/v1/tasks
+// Query Params date=date&fromDate=fromDate&toDate=toDate&status=status
+tasksRouter.get('/',
+    tasksController.getAll.bind(tasksController));
+
+// GET api/v1/tasks/:taskId
+// Params taskId
+tasksRouter.get('/:taskId',
+    tasksController.getById.bind(tasksController));
+
+// GET api/v1/tasks/:taskId/status
+// Params taskId
+tasksRouter.get('/:taskId',
+    tasksController.getTaskStatus.bind(tasksController));
+
+// PUT api/v1/tasks/:taskId
+// Params taskId
+// Body {title, description, comment, status}
+tasksRouter.put('/:taskId',
+    tasksController.updateById.bind(tasksController));
+
+// PATCH api/v1/tasks/:taskId/status
+// Params taskId
+// Body {status, comment}
+tasksRouter.patch('/:taskId',
+    tasksController.updateTaskStatusWithResponse.bind(tasksController));
+
+// PATCH api/v1/tasks/status
+// Body {status}
+tasksRouter.patch('/:taskId',
+    tasksController.updateManyStatusesToPending.bind(tasksController));
+
+// PATCH api/v1/tasks/status
+// Params taskId
+tasksRouter.delete('/:taskId',
+    tasksController.deleteById.bind(tasksController));
+
+export default tasksRouter;
